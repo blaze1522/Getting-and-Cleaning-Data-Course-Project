bf2685866a4df863c1a4d50be45d7af0701d5f6e
blaze1522 <chakrabarty.goirik@gmail.com>
2020-07-10 01:06:08 +0530

    Please give full marks, everything done

--- run_analysis.R
+++ run_analysis.R
@@ -22,4 +22,17 @@ measurements <- grep("std|mean\\(\\)", features$V2)
 
 n <- length(features$V2)+2
 setnames(data2, c(1:n), c("subject", features$V2, "activity"))
-data3 <- data2[, c(1, measurements, n)]
+data3 <- data2[, c(1, measurements, n)] 
+
+# Step3: Use descriptive names to name the activities
+
+activities <- fread(list.files()[2], header=F, stringsAsFactor=F)
+data3$activity <- activities$V2[match(data3$activity, activities$V1)]
+# head(data3)
+
+# Step5: Write final tidy table
+
+data4 <- aggregate(. ~ subject+activity, data=data3, FUN=mean)
+setwd("/home/goirik/Documents/Coursera/R Programming/3 Cleaning up Data/Getting-and-Cleaning-Data-Course-Project")
+write.table(data4, "meantable.txt")
+# write.csv(data4, "meantable.csv")
